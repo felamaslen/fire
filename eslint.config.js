5068a135b8238f85152474379cc1c984010fc838
Fela Maslen <felamaslen@gmail.com>
2026-04-16 22:22:47 +0100

    chore(lint): add eslint and prettier configs

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import js from "@eslint/js";
+import prettier from "eslint-plugin-prettier/recommended";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/build/**",
+      "**/__generated__/**",
+    ],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: { "simple-import-sort": simpleImportSort },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+  prettier,
+);
