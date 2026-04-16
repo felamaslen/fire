593d82b7363435d84657d6713df3c99ab057d57e
Fela Maslen <felamaslen@gmail.com>
2026-04-16 23:43:43 +0100

    test: silence passed tests

--- packages/backend/vite.config.ts
+++ packages/backend/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "#test": fileURLToPath(new URL("./test", import.meta.url)),
     },
   },
   ssr: {
@@ -15,6 +16,7 @@ export default defineConfig({
     globalSetup: ["./test/global-setup.ts"],
     setupFiles: ["./test/setup.ts"],
     pool: "forks",
+    silent: "passed-only",
     env: {
       DATABASE_URL: "postgres://fire:fire@localhost:5433/postgres",
     },
