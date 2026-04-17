3cb8af81514ac417f22913f098740b16df0931f2
Fela Maslen <felamaslen@gmail.com>
2026-04-17 20:19:41 +0100

    fix: use vite-node-app to serve backend

---
+++ packages/backend/src/app.ts
@@ -0,0 +1,7 @@
+import "./graphql/server";
+import "./graphql/schema-download";
+import "./uploads";
+
+import { router } from "./router";
+
+export const viteNodeApp = router;
--- packages/backend/vite.config.ts
+++ packages/backend/vite.config.ts
@@ -1,33 +1,50 @@
 import { fileURLToPath } from "node:url";
 
 import { loadEnv } from "vite";
+import { VitePluginNode } from "vite-plugin-node";
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-      "#test": fileURLToPath(new URL("./test", import.meta.url)),
+export default defineConfig(({ mode }) => {
+  Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
+
+  return {
+    server: {
+      port: 4000,
+      host: "0.0.0.0",
     },
-    dedupe: ["graphql"],
-  },
-  ssr: {
-    noExternal: [
-      "graphql",
-      "@apollo/server",
-      "@as-integrations/fastify",
-      "@graphql-tools/schema",
-      "@graphql-tools/merge",
-      "@graphql-tools/utils",
-      "@graphql-tools/executor",
+    plugins: [
+      ...VitePluginNode({
+        adapter: "fastify",
+        appPath: "./src/app.ts",
+        exportName: "viteNodeApp",
+        tsCompiler: "vite",
+      }),
     ],
-  },
-  test: {
-    globalSetup: ["./test/global-setup.ts"],
-    setupFiles: ["./test/setup.ts"],
-    pool: "forks",
-    globals: true,
-    silent: "passed-only",
-    env: loadEnv("test", process.cwd(), ""),
-  },
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+        "#test": fileURLToPath(new URL("./test", import.meta.url)),
+      },
+      dedupe: ["graphql"],
+    },
+    ssr: {
+      noExternal: [
+        "graphql",
+        "@apollo/server",
+        "@as-integrations/fastify",
+        "@graphql-tools/schema",
+        "@graphql-tools/merge",
+        "@graphql-tools/utils",
+        "@graphql-tools/executor",
+      ],
+    },
+    test: {
+      globalSetup: ["./test/global-setup.ts"],
+      setupFiles: ["./test/setup.ts"],
+      pool: "forks",
+      globals: true,
+      silent: "passed-only",
+      env: loadEnv("test", process.cwd(), ""),
+    },
+  };
 });
