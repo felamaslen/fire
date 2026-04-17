aeefcda9cd1d1b487754a4869745e5ec3c3733c9
Fela Maslen <felamaslen@gmail.com>
2026-04-17 20:19:41 +0100

    fix: use vite-node-app to serve backend

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,16 +1,12 @@
-import "./graphql/server";
-import "./graphql/schema-download";
-import "./uploads";
-
+import { viteNodeApp } from "./app";
 import { env } from "./env";
-import { router } from "./router";
 
 if (env.NODE_ENV !== "test") {
-  await router.listen({ port: env.PORT, host: "0.0.0.0" });
+  await viteNodeApp.listen({ port: env.PORT, host: "0.0.0.0" });
 
   for (const signal of ["SIGINT", "SIGTERM", "SIGUSR2"] as const) {
     process.once(signal, () => {
-      void router.close().then(() => {
+      void viteNodeApp.close().then(() => {
         process.kill(process.pid, signal);
       });
     });
