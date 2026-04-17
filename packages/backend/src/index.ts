fc1abf71a3d50443dd2877f6b09179468f1f7541
Fela Maslen <felamaslen@gmail.com>
2026-04-17 20:03:01 +0100

    feat: graceful shutdown

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -7,4 +7,12 @@ import { router } from "./router";
 
 if (env.NODE_ENV !== "test") {
   await router.listen({ port: env.PORT, host: "0.0.0.0" });
+
+  for (const signal of ["SIGINT", "SIGTERM", "SIGUSR2"] as const) {
+    process.once(signal, () => {
+      void router.close().then(() => {
+        process.kill(process.pid, signal);
+      });
+    });
+  }
 }
