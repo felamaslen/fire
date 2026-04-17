a7b903bb740d74859bf29aa02ee4b85ee478226f
Fela Maslen <felamaslen@gmail.com>
2026-04-17 21:32:49 +0100

    feat: implement possible types for caching

--- packages/web/src/apollo.ts
+++ packages/web/src/apollo.ts
@@ -1,10 +1,12 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
+import { possibleTypes } from "./__generated__/possible-types";
+
 export function createApolloClient() {
   return new ApolloClient({
     link: new HttpLink({
       uri: import.meta.env.VITE_GRAPHQL_URL ?? "http://localhost:4000/graphql",
     }),
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache({ possibleTypes }),
   });
 }
