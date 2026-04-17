4f9ec0abd1415d2a4a8dc0b9622fb253e64d9d90
Fela Maslen <felamaslen@gmail.com>
2026-04-17 20:22:50 +0100

    feat(web): apollo client

---
+++ packages/web/src/apollo.ts
@@ -0,0 +1,10 @@
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+
+export function createApolloClient() {
+  return new ApolloClient({
+    link: new HttpLink({
+      uri: import.meta.env.VITE_GRAPHQL_URL ?? "http://localhost:4000/graphql",
+    }),
+    cache: new InMemoryCache(),
+  });
+}
