08fc3feb1a5417b7e8fe7439b821a786262f61ab
Fela Maslen <felamaslen@gmail.com>
2026-04-19 00:01:24 +0100

    fix: winston logs not showing

--- packages/backend/src/router.ts
+++ packages/backend/src/router.ts
@@ -1,5 +1,8 @@
 import Fastify from "fastify";
 
 import { env } from "./env";
+import { fastifyLogger } from "./log";
 
-export const router = Fastify({ logger: env.NODE_ENV !== "test" });
+export const router = Fastify({
+  loggerInstance: env.NODE_ENV === "test" ? undefined : fastifyLogger,
+});
