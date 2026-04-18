7e08e486a09b77ec88364703247f6c7aa42de416
Fela Maslen <felamaslen@gmail.com>
2026-04-18 16:16:34 +0100

    feat(web): payslip PDF upload and preview

--- packages/web/src/apollo.ts
+++ packages/web/src/apollo.ts
@@ -1,11 +1,13 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import UploadHttpLink from "apollo-upload-client/UploadHttpLink.mjs";
 
 import { possibleTypes } from "./__generated__/possible-types";
 
 export function createApolloClient() {
   return new ApolloClient({
-    link: new HttpLink({
+    link: new UploadHttpLink({
       uri: import.meta.env.VITE_GRAPHQL_URL ?? "http://localhost:4000/graphql",
+      headers: { "apollo-require-preflight": "true" },
     }),
     cache: new InMemoryCache({ possibleTypes }),
   });
